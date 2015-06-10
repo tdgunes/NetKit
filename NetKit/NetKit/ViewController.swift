24d1b84149d8d18d716b73ab1b289634aad8daa3
tdgunes <tdgunes@gmail.com>
2015-06-10 23:04:47 +0300

    Initial files for testing

---
+++ NetKit/NetKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetKit
+//
+//  Created by Taha Doğan Güneş on 10/06/15.
+//  Copyright (c) 2015 Taha Doğan Güneş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
